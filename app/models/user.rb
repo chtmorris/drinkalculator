b6113a1be87f131e4637249a4bba6b8250a515f0
Screen Goose <sarrieschaz@hotmail.com>
2014-03-12 15:42:48 +0800

    Added bcrypt gem and fields to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
+require 'bcrypt'
+
 class User
 
   include Mongoid::Document
   include Mongoid::Timestamps
 
   field :email, type: String
+  field :salt, type: String
+  field :fish, type: String
 
 end
