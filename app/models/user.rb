8ce6d84611e1b7c2302bd881e35048bacaa970d4
Screen Goose <sarrieschaz@hotmail.com>
2014-03-12 17:06:54 +0800

    Added password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,21 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessor :password, :password_confirmation
+
+  before_save :encrypt_password
+
   field :email, type: String
   field :salt, type: String
   field :fish, type: String
+
+  protected
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.fish = BCrypt::Engine.hash_secret(password, self.salt)
+    end
+  end
 
 end
