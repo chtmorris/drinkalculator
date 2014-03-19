5357fd0c64395e09d341ebbec8389b1925ed8855
Screen Goose <sarrieschaz@hotmail.com>
2014-03-19 10:48:00 +0800

    amended password controller

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -1,17 +1,19 @@
 class PasswordController < ApplicationController
 
+  LINK_EXPIRED = "Your reset link has expired. Please generate a new one."
+
   def edit
-    @user = User.find_by code: params[:code]
-    redirect_to login_url, notice: "Your reset link has expired. Please generate a new one." unless @user
+    unless @user = User.find_by_code( params[:code] )
+      redirect_to login_url, notice: LINK_EXPIRED
+    end
   end
 
   def update
-    @user = User.find_by_code params[:code]
-
-    if @user
-      #if user is found
+    if @user = User.find_by_code( params[:code] )
+    #if user is found
       if params[:user][:password].blank?
-        @user.errors.add(:password, "This field can't be blank")
+
+        @user.errors.add(:password, "can't be blank")
       elsif @user.update_attributes user_params
         render text: "Success"
       else
