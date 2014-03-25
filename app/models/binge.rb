f1a0611837887f29e6f99715733638a2871eb91a
Screen Goose <sarrieschaz@hotmail.com>
2014-03-25 09:23:45 +0800

    Minor code cleanup

--- app/models/binge.rb
+++ app/models/binge.rb
@@ -8,23 +8,5 @@ class Binge
 
   embedded_in :user
 
-  def date (params)
-    if params[:date] = nil
-      Time.now.strftime("%m/%d/%Y")
-      self.save
-    else
-      params[:date]
-      self.save
-    end
-  end
-
-  # (if params[:date] = nil then (Time.now.strftime("%m/%d/%Y")) else params[:date])
-
-  # def add_beer (params)
-  #   input_time = Time.now.strftime("%m/%d/%Y")
-  #   self.write_attribute(input_time, params.to_i)
-  #   self.save
-  # end
-
 
 end
